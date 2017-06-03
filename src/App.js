389ee0d00514b6a414ddb9e309dd6ee891e724e5
Yichuan Shen <shen.yichuan@gmail.com>
2017-06-03 15:54:39 +0200

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {h, Component} from 'preact'
+
+import Outliner from './Outliner'
+
+export default class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            content: [
+                '# Title',
+                '',
+                '- [ ] Hello World!',
+                '    - [x] Hello',
+                '          With some description',
+                '    - [ ] World!'
+            ].join('\n')
+        }
+
+        this.handleOutlinerChange = evt => {
+            this.setState({content: evt.content})
+        }
+    }
+
+    render() {
+        return <section id="app">
+            <Outliner content={this.state.content} onChange={this.handleOutlinerChange} />
+        </section>
+    }
+}
