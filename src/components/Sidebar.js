397128f5c7c3c2d85335f1f4aae288aa4422e788
Yichuan Shen <shen.yichuan@gmail.com>
2017-06-22 22:33:23 +0200

    Better drag style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,10 @@
 import {h, Component} from 'preact'
 import classNames from 'classnames'
 
+function cap(min, max, value) {
+    return Math.min(max, Math.max(min, value))
+}
+
 export default class Sidebar extends Component {
     constructor() {
         super()
@@ -8,7 +12,8 @@ export default class Sidebar extends Component {
         this.state = {
             dragging: false,
             dragIndex: 0,
-            dragBefore: 0
+            dragBefore: 0,
+            dragGhostTop: 0
         }
 
         this.handleResizerMouseDown = evt => {
@@ -24,11 +29,17 @@ export default class Sidebar extends Component {
             if (evt.button !== 0) return
 
             let {currentTarget} = evt
+            let ulElement = currentTarget.parentNode.parentNode
+            let {top: itemTop, height: itemHeight} = currentTarget.getBoundingClientRect()
+            let {top, bottom} = ulElement.getBoundingClientRect()
 
             this.itemDragged = false
             this.itemMouseDown = {
                 index: +currentTarget.parentNode.dataset.index,
-                element: currentTarget
+                diff: evt.clientY - itemTop,
+                itemHeight,
+                top,
+                bottom
             }
         }
 
@@ -75,16 +86,14 @@ export default class Sidebar extends Component {
 
                 this.itemDragged = true
 
-                let {element} = this.itemMouseDown
-                let ulElement = element.parentNode.parentNode
-                let {height} = element.getBoundingClientRect()
-                let {top} = ulElement.getBoundingClientRect()
-                let dragBefore = Math.floor((evt.clientY - top) / height)
+                let {diff, itemHeight, top, bottom} = this.itemMouseDown
+                let dragBefore = Math.floor((evt.clientY - top) / itemHeight)
 
                 this.setState({
                     dragging: true,
                     dragIndex: this.itemMouseDown.index,
-                    dragBefore: Math.min(Math.max(dragBefore, 0), this.props.items.length - 1)
+                    dragBefore: cap(0, this.props.items.length - 1, dragBefore),
+                    dragGhostTop: cap(top, bottom - itemHeight, evt.clientY - diff)
                 })
             }
         })
@@ -97,6 +106,7 @@ export default class Sidebar extends Component {
             || this.state.dragging !== nextState.dragging
             || this.state.dragIndex !== nextState.dragIndex
             || this.state.dragBefore !== nextState.dragBefore
+            || this.state.dragGhostTop !== nextState.dragGhostTop
     }
 
     render() {
@@ -117,7 +127,7 @@ export default class Sidebar extends Component {
                     </a>
                 </h3>
 
-                <ul>
+                <ul class={classNames({dragging: this.state.dragging})}>
                 {items.map(([i, name]) =>
                     <li key={i} data-index={i} class={classNames({
                         selected: this.props.selected === i,
@@ -129,10 +139,26 @@ export default class Sidebar extends Component {
                             onMouseDown={this.handleItemMouseDown}
                         >
                             <img src="./node_modules/octicons/build/svg/file.svg" />
-                            {name === '' ? '(Untitled)' : name}
+                            {name || '(Untitled)'}
                         </a>
                     </li>
                 )}
+
+                {this.state.dragging &&
+                    <li
+                        key="ghost"
+                        style={{top: this.state.dragGhostTop}}
+                        class={classNames({
+                            ghost: true,
+                            selected: this.props.selected === this.state.dragIndex
+                        })}
+                    >
+                        <a href="#">
+                            <img src="./node_modules/octicons/build/svg/file.svg" />
+                            {this.props.items[this.state.dragIndex] || '(Untitled)'}
+                        </a>
+                    </li>
+                }
                 </ul>
             </nav>
 
