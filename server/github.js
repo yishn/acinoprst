8d5b05921ec5b354a0057b8239cc07e2bdb08b37
Yichuan Shen <shen.yichuan@gmail.com>
2017-07-10 11:32:17 +0200

    Add github.(remove|create)Gist() and github.syncAcinoprstGist()

--- server/github.js
+++ server/github.js
@@ -30,7 +30,32 @@ module.exports = ({client_id, client_secret}) => ({
             qs: options,
             json: true,
             headers: makeHeaders(access_token)
-        }, (err, _, data) => {
+        }, (err, response, data) => {
+            if (response.statusCode >= 300) return callback(new Error())
+            if (err) return callback(err)
+            callback(null, data)
+        })
+    },
+
+    removeGist(access_token, id, callback) {
+        request.delete({
+            url: `https://api.github.com/gists/${id}`,
+            headers: makeHeaders(access_token)
+        }, (err, response) => {
+            if (response.statusCode >= 300) return callback(new Error())
+            if (err) return callback(err)
+            callback(null)
+        })
+    },
+
+    createGist(access_token, options, callback) {
+        request.post({
+            url: 'https://api.github.com/gists',
+            form: options,
+            json: true,
+            headers: makeHeaders(access_token)
+        }, (err, response, data) => {
+            if (response.statusCode >= 300) return callback(new Error())
             if (err) return callback(err)
             callback(null, data)
         })
@@ -39,9 +64,7 @@ module.exports = ({client_id, client_secret}) => ({
     getAcinoprstGist(access_token, callback) {
         this.getGists(access_token, {per_page: 100}, (err, data) => {
             if (err) return callback(err)
-
             err = new Error('Not found')
-            if (data.message != null) return callback(err)
 
             let gist = data.find(x => x.description === 'acinoprst')
             if (gist == null) return callback(err)
@@ -58,6 +81,18 @@ module.exports = ({client_id, client_secret}) => ({
                 file.content = data
                 callback(null, gist)
             })
+        })
+    },
+
+    syncAcinoprstGist(access_token, oldId, content, callback) {
+        this.removeGist(access_token, oldId, err => {
+            if (err) return callback(err)
+
+            this.createGist(access_token, {
+                description: 'acinoprst',
+                public: false,
+                files: {'acinoprst.md': {content}}
+            }, callback)
         })
     }
 })
