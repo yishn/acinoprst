d400b64c5f96830cddbe691c643e9188a3a4d8df
Yichuan Shen <shen.yichuan@gmail.com>
2017-07-16 16:44:47 +0200

    Clean dependencies

--- client/index.js
+++ client/index.js
@@ -1,21 +1,5 @@
 import querystring from 'querystring'
-import cookies from 'js-cookie'
 import {h, render} from 'preact'
 import App from './components/App'
 
-let query = querystring.parse(window.location.search.slice(1))
-let noQueryHref = window.location.href.replace(window.location.search, '')
-
-if (query.access_token != null) {
-    // Set token cookie
-
-    cookies.set('oauth_token', query.access_token, {path: ''})
-    window.location.replace(noQueryHref)
-} else if (query.logout != null) {
-    // Remove token cookie
-
-    cookies.remove('oauth_token', {path: ''})
-    window.location.replace(noQueryHref)
-} else {
-    render(<App/>, document.body)
-}
+render(<App/>, document.body)
