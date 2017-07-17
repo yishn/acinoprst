6858fb29d528ba1fc757b0fe6adbcbdcd3eb854e
Yichuan Shen <shen.yichuan@gmail.com>
2017-07-17 20:30:11 +0200

    Prepare

--- src/components/Busy.js
+++
@@ -1,8 +0,0 @@
-import {h, Component} from 'preact'
-import classNames from 'classnames'
-
-export default class Busy extends Component {
-    render() {
-        return <div class={classNames({show: this.props.show})} id="busy"/>
-    }
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'regenerator-runtime/runtime'
-import querystring from 'querystring'
-import {h, render} from 'preact'
-import App from './components/App'
-
-render(<App/>, document.body)
