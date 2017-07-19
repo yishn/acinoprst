ce4c4b4f4ccaedbc998d398cca8e67f38b2e797d
Yichuan Shen <shen.yichuan@gmail.com>
2017-07-19 12:19:47 +0200

    Add Promise polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import 'regenerator-runtime/runtime'
+import Promise from 'pinkie-promise'
+window.Promise = Promise
+
 import querystring from 'querystring'
 import {h, render} from 'preact'
 import App from './components/App'
