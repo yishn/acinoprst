d5e3c6c2a5e350939bdf7de3c619e57661517b1b
Yichuan Shen <shen.yichuan@gmail.com>
2018-01-30 19:30:43 +0100

    Add basic files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+
+    devtool: 'source-map',
+
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                query: {
+                    presets: [['env', {modules: false}], 'stage-1'],
+                    plugins: ['fast-async', ['transform-react-jsx', {pragma: 'h'}]]
+                }
+            }
+        ]
+    },
+
+    resolve: {
+        alias: {
+            'preact': path.join(__dirname, 'node_modules/preact/dist/preact.min')
+        }
+    }
+}
