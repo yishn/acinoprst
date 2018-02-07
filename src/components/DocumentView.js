e6819950bc3a398a2d4616699a6a37ee165a81ed
Yichuan Shen <shen.yichuan@gmail.com>
2018-02-07 21:05:25 +0100

    Visualize progress in the header

--- src/components/DocumentView.js
+++ src/components/DocumentView.js
@@ -1,12 +1,33 @@
 import {h, Component} from 'preact'
 import classnames from 'classnames'
+import * as outline from '../outline'
 import OutlineView from './OutlineView'
 
 class DocumentViewHeader extends Component {
+    state = {
+        progress: 0
+    }
+
+    componentWillReceiveProps(nextProps) {
+        let {doc} = this.props
+
+        clearTimeout(this.updateProgressId)
+
+        this.updateProgressId = setTimeout(() => {
+            if (doc.list === this.props.doc.list) return
+
+            let {progress} = outline.getStats(this.props.doc.list)
+            this.setState({progress: Math.round(progress * 100)})
+        }, 500)
+    }
+
     render() {
+        let {progress} = this.state
         let {doc} = this.props
 
         return <div class="document-view-header">
+            <div class="progress" style={{width: `${progress}%`}}/>
+
             <h1>{doc.title}</h1>
         </div>
     }
