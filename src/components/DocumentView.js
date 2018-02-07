a497a34ca4bb7379c4d64d75e1b5739ec3c5b6d7
Yichuan Shen <shen.yichuan@gmail.com>
2018-02-07 22:26:48 +0100

    Editable document title

--- src/components/DocumentView.js
+++ src/components/DocumentView.js
@@ -5,7 +5,7 @@ import OutlineView from './OutlineView'
 
 class DocumentViewHeader extends Component {
     state = {
-        progress: 0
+        progress: null
     }
 
     componentWillReceiveProps(nextProps) {
@@ -14,11 +14,22 @@ class DocumentViewHeader extends Component {
         clearTimeout(this.updateProgressId)
 
         this.updateProgressId = setTimeout(() => {
-            if (doc.list === this.props.doc.list) return
+            if (this.state.progress != null && doc.list === this.props.doc.list) return
 
             let {progress} = outline.getStats(this.props.doc.list)
             this.setState({progress: Math.round(progress * 100)})
         }, 500)
+    }
+
+    componentDidMount() {
+        this.componentWillReceiveProps(this.props)
+    }
+
+    handleInput = evt => {
+        let {onChange = () => {}} = this.props
+        let {value} = evt.currentTarget
+
+        onChange({title: value})
     }
 
     render() {
@@ -28,7 +39,7 @@ class DocumentViewHeader extends Component {
         return <div class="document-view-header">
             <div class="progress" style={{width: `${progress}%`}}/>
 
-            <h1>{doc.title}</h1>
+            <h1><input value={doc.title} onInput={this.handleInput}/></h1>
         </div>
     }
 }
@@ -44,7 +55,10 @@ export default class DocumentView extends Component {
         let {doc, selectedIds} = this.props
 
         return <section class="document-view">
-            <DocumentViewHeader doc={doc} />
+            <DocumentViewHeader
+                doc={doc}
+                onChange={this.handleChange}
+            />
 
             <OutlineView
                 list={doc.list}
