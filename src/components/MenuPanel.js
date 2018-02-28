e58d9605d86c502c7dd517b0941bc8ed58d62f18
Yichuan Shen <yichuan.shen@sap.com>
2018-02-28 17:25:45 +0100

    Add login form

--- src/components/MenuPanel.js
+++ src/components/MenuPanel.js
@@ -26,7 +26,7 @@ export default class MenuPanel extends Component {
     render() {
         let {user, show, docs, currentIndex} = this.props
 
-        return <section class={classnames('menu-panel', {show})}>
+        return <section class={classnames('menu-panel', {show, login: user == null})}>
             {user != null ? (
                 <div>
                     <div class="user">
@@ -40,7 +40,7 @@ export default class MenuPanel extends Component {
                         <ToolbarButton icon="./img/logout.svg" text="Logout" onClick={this.props.onLogout}/>
                     </Toolbar>
 
-                    <ol class="documents">{docs.map((doc, i) => 
+                    <ol class="documents">{docs.map((doc, i) =>
                         <DocumentListItem
                             index={i}
                             current={i === currentIndex}
@@ -49,9 +49,29 @@ export default class MenuPanel extends Component {
                             onClick={this.props.onDocumentClick}
                         />
                     )}</ol>
-                </div> 
+                </div>
             ) : (
                 <div>
+                    <div class="user">
+                        <img src="./img/github.svg"/>
+                        <h2>Login</h2>
+                    </div>
+
+                    <form>
+                        <ul>
+                            <li>
+                                <label for="input-gist-url">Gist URL:</label>
+                                <input id="input-gist-url"/>
+                            </li>
+                            <li>
+                                <label for="input-access-token">Access Token:</label>
+                                <input id="input-access-token"/>
+                            </li>
+                            <li class="buttons">
+                                <button>Login</button>
+                            </li>
+                        </ul>
+                    </form>
                 </div>
             )}
         </section>
